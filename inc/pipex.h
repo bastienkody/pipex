8dcb3a642c7e69cf10402bd604d385f504e328be
Bastien Guillaume <bguillau@student.42.fr>
2023-02-27 20:21:14 +0100

    struct pour parsing

--- inc/pipex.h
+++ inc/pipex.h
@@ -17,11 +17,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef	struct files
+{
+	char	*infile;
+	char	*outfile;
+	int		here_doc;
+	char	*limiter;
+}				t_files;
+
+typedef	struct s_cmd
+{
+	char	*cmd_name;	// name (ls)
+	char	*cmd_path;	// full path (/ur/bin/ls)
+	char	**cmd_argv;	// options + arg avec cmd_name en argv[0]
+	int		exist;		// F_OK
+	int		is_exec;	// X_OK
+}				t_cmd;
 
 /* aux */
 void	print_path(t_list *path);
+void	print_files(t_files files);
 
 /* parsing */
 t_list	*path_to_llist(char **envp);
-
+t_files	file_parser(int argc, char **argv);
 #endif
