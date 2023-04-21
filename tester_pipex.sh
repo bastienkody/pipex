f802f1e941369d29b6d3a06c4f0f419705e3ebbb
Bastien Guillaume <bguillau@student.42.fr>
2023-04-21 16:43:24 +0200

    testeur

--- tester_pipex.sh
+++ tester_pipex.sh
@@ -33,7 +33,6 @@ echo "Test pb nombre arg (0, 1, 2 et 3):"
 ./pipex Makefile "echo yo" > two_arg.txt 2> two_arg_err.txt
 ./pipex Makefile "echo yo" "echo yi" > three_arg.txt 2> three_arg_err.txt
 ./pipex Makefile "echo yo" "echo yi" outfile > four_arg.txt 2> four_arg_err.txt
-
 if [[ -s no_arg.txt && -s one_arg.txt && -s two_arg.txt && -s three_arg.txt ]] ;
 then	echo "${YEL}pipex ecrit sur std out (fd 1)${END}"
 fi
@@ -43,14 +42,14 @@ fi
 if [[ $(cat no_arg.txt no_arg_err.txt one_arg.txt one_arg_err.txt two_arg.txt two_arg_err.txt three_arg.txt three_arg_err.txt | egrep "(yi|yo)") ]] ;
 then	echo "${RED}pipex realise les commandes${END}"
 fi
-rm -rf *arg*.txt
+rm -rf *arg*.txt outfile
 
 #basics tests
 echo "${ITA}Test basics:${END}"
 
-echo -n "Test 1 : ./pipex Makefile ls /usr/bin/ls t1_output \t--> "
-touch t1_output t1_expected
-/usr/bin/ls > t1_expected
+echo -n "Test 1 : ./pipex Makefile ls ls t1_output \t--> "
+touch t1_output t1_expected #both created here bc of ls cmd
+ls > t1_expected
 ./pipex "Makefile" "ls" "/usr/bin/ls" "t1_output" 
 diff t1_expected t1_output && {echo "${GREEN}OK${END}" && rm t1_*} || echo "${RED}KO${END}"
 
@@ -70,8 +69,76 @@ echo "${ITA}Tests infile pb${END}"
 touch infile_r infile_no_r && chmod u-r infile_no_r
 
 echo -n "Test 1 : ./pipex infile_r touch truc touch truc2 t1_output \t--> "
-./pipex "infile_r" "touch truc" "touch truc2" "t1_output" 
-[[ $(ls -l | egrep "truc2?" | wc -l) -eq 2 ]] && {echo "${GREEN}OK${END}" && rm t1_*} || echo "${RED}KO${END}"
-rm truc*
+./pipex infile_r "touch truc" "touch truc2" t1_output
+[[ $(ls -l | egrep "truc2?" | wc -l) -eq 2 ]] && {echo "${GREEN}OK${END}" ; rm t1_* truc*} || echo "${RED}KO${END}"
+
+echo -n "Test 2 : ./pipex infile_no_r touch NON touch OUI t2_output \t--> "
+./pipex infile_no_r "touch NOT" "touch OUI" t2_output 2> stderr.txt
+[[ !$(ls -l | grep "NOT") && $(ls -l | grep "OUI") ]] && {echo -n "${GREEN}OK${END}" ; rm t2_* OUI} || echo -n "${RED}KO${END}"
+[[ $(cat stderr.txt | grep -i "permission denied") ]] && echo "${GREEN} (+ err msg ok)${END}" || echo "${YEL} (- err msg without \"Permission denied\")${END}"
+rm stderr.txt infile*
+
+#outfile pb tests
+echo "${ITA}Tests outfile pb${END}"
+touch outfile_w outfile_no_w && chmod u-w outfile_no_w
+
+echo -n "Test 1 : ./pipex Makefile touch truc touch truc2 outfile_w \t--> "
+./pipex Makefile "touch truc" "touch truc2" outfile_w
+[[ $(ls -l | egrep "truc2?" | wc -l) -eq 2 ]] && {echo "${GREEN}OK${END}" ; rm -f truc*} || echo "${RED}KO${END}"
+
+echo -n "Test 2 : ./pipex Makefile touch OUI touch NON outfile_no_w \t--> "
+./pipex Makefile "touch OUI" "touch NON" outfile_no_w 2> stderr.txt
+code=$(echo $?)
+[[ !$(ls -l | grep "NOT") && $(ls -l | grep "OUI") ]] && {echo -n "${GREEN}OK${END}" ; rm OUI} || echo -n "${RED}KO${END}"
+[[ $(cat stderr.txt | grep -i "permission denied") ]] && echo -n "${GREEN} (+ err msg ok)${END}" || echo -n "${YEL} (- err msg without \"Permission denied\")${END}"
+[[ $code -eq 1 ]] && echo "${GREEN}(+ return status == 1 ok)${END}" || echo "${YEL}(- return status != 1 ko)${END}"
+rm -f stderr.txt outfile*
+
+#silmultaneous cmd test
+echo "${ITA}Tests cmd silmutaneous${END}"
+
+echo -n "Test 1 : ./pipex Makefile yes "echo yo" outf \t--> "
+./pipex Makefile yes "echo yo" outf
+[[ $(cat outf) -eq "yo" ]] && {echo "${GREEN}OK${END}" ; rm -f outf} || echo "${RED}KO${END}"
+
+echo -n "Test 2 : ./pipex Makefile "sleep 2" "sleep 1" outf \t--> "
+t1=$(date +%s)
+./pipex Makefile "sleep 2" "sleep 1" outf
+t2=$(date +%s)
+[[ $((t2 - t1)) -eq 2 ]] && {echo "${GREEN}OK${END}" ; rm -f outf} || echo "${RED}KO${END}"
+
+#echo -n "Test 3 : ./pipex Makefile "yes" "cati" outf \t--> "
+
+#cmd with absolute path
+echo "${ITA}Tests cmd absolut path${END}"
+
+echo -n "Test 1 : ./pipex Makefile /usr/bin/ls /usr/bin/cat t1_output \t--> "
+touch t1_output t1_expected
+/usr/bin/ls < Makefile | /usr/bin/cat > t1_expected
+./pipex "Makefile" "/usr/bin/ls" "/usr/bin/cat" "t1_output" 
+diff t1_expected t1_output && {echo "${GREEN}OK${END}" && rm t1_*} || echo "${RED}KO${END}"
+
+#non existing cmd w/ abs. path
+echo "${ITA}Tests no exist cmd absolut path${END}"
+
+echo -n "Test 1 : ./pipex Makefile /usr/bin/lsoip /usr/bin/cati outf \t--> "
+/usr/bin/lsoip < Makefile 2>/dev/null | /usr/bin/cati > t1_expected 2>/dev/null
+./pipex "Makefile" "/usr/bin/lsoip" "/usr/bin/cati" "outf" 2> stderr.txt
+[[ -s stderr.txt ]] && {echo -n "${GREEN}OK${END}" && rm -f outf} || echo -n "${RED}KO${END}"
+[[ $(cat stderr.txt | egrep -i "(file|directory)") ]] && echo -n "${GREEN} (+ err msg ok)${END}" || echo -n "${YEL} (- err msg without \"No such file or directory\")${END}"
+# + verif code erreur
+
+#non existing cmd w/ rel. path
+
+
+
+
+
+
+
+
+
+
+#executable
 
 
